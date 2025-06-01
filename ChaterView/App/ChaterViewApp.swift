309d41ce38caf519161c83b5b3d14e2d819b6035
ghrjs1998 <zbsl4547@naver.com>
2025-06-01 20:40:35 +0900

    카카오로그인에러픽스

--- ChaterView/App/ChaterViewApp.swift
+++ ChaterView/App/ChaterViewApp.swift
@@ -15,6 +15,7 @@ class AppDelegate: NSObject, UIApplicationDelegate {
         FirebaseApp.configure()
         
         if let kakaoAppKey = Bundle.main.object(forInfoDictionaryKey: "KAKAO_APP_KEY") as? String {
+            print("✅ Kakao App Key: \(kakaoAppKey)")
             KakaoSDK.initSDK(appKey: kakaoAppKey)
         } else {
             print("❌ Kakao App Key 로딩 실패 - Info.plist 확인 필요")
